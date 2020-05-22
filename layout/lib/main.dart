3676d1f620f916e82ed34dfd339e8a34bea94128
qwertymoscar <moussa4.sow@gmail.com>
2020-05-22 16:26:02 +0200

    State full

--- layout/lib/main.dart
+++ layout/lib/main.dart
@@ -107,13 +107,52 @@ Widget titleSection = Container(
         ),
       ),
       /*3*/
-      Icon(
-        Icons.star,
-        color: Colors.red[500],
-      ),
-      Text('41'),
+      FavoriteWidget(),
     ],
   ),
 );
 
 
+class FavoriteWidget extends StatefulWidget{
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget>{
+  bool _isFavorited = true;
+  int _favoriteCount = 39;
+
+  @override
+  Widget build(BuildContext context){
+    void _toggleFavorite(){
+      setState(() {
+        if (_isFavorited) {
+          _favoriteCount--;
+          _isFavorited = false;
+        } else {
+          _favoriteCount++;
+          _isFavorited = true;
+        }
+      });
+    }
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+            ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount')
+          ),
+        )
+      ],
+    );
+  }
+}
